b205663dbfce96a9ed5fe7c49dd95cb2cea133a0
Sunly <sunjingxiang1995@gmail.com>
2018-08-08 23:27:50 +0800

    动态获取天气信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,36 @@
-Page({})
+const weatherName = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪',
+  'overcast': '阴天',
+}
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: ''
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: "兰州市",
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        console.log(res.data)
+        let temp = res.data.result.now.temp
+        let weather = res.data.result.now.weather
+        console.log(temp,weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherName[weather],
+        })
+      }
+    })
+  }
+})
