a0fe4e9ca67e4bed949a4c59b4e61d00d3166838
Sunly <sunjingxiang1995@gmail.com>
2018-08-26 02:04:03 +0800

    临时保存。天气api，请求网络数据，渲染未来天气

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,2 +1,36 @@
-// pages/list/list.js
-Page({})
+//天气名称对应
+const weatherName = {
+  'cloudy': '阴天',
+  'heavyrain': '大雨',
+  'lightrain': '小雨',
+  'overcast': '多云',
+  'snow': '下雪',
+  'sunny': '晴天',
+}
+
+Page({
+  onLoad(){
+    this.getNow()
+  },
+  data: {
+    week: '',
+    date: '',
+    temp: '',
+    forecastItem: [1,2,3,4,5,6,7],
+  },
+  getNow(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data: {
+        city: '北京市',
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        let result = res.data
+        console.log(result)
+      }
+    })
+  }
+})
